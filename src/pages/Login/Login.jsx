8bdb723ee7ccaeb9c03f3c44de96055a2d09fe35
Naharun <naharunsraboni@gmail.com>
2023-05-19 23:08:47 +0600

    done login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,23 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 
 const Login = () => {
-    const handleLogin = event =>{
+    const { signIn } = useContext(AuthContext)
+    const handleLogin = event => {
         event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password)
+        const from = event.target;
+        const email = from.email.value;
+        const password = from.password.value;
+        console.log(email, password);
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error))
+
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -15,27 +25,27 @@ const Login = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h2 className="text-2xl font-bold">Login Now</h2>
-                        <form>
+                        <form onSubmit={handleLogin}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="text" name="email" placeholder="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="text" name="password" placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                              <input className="btn btn-warning" type="submit" value="login" />
+                                <input className="btn btn-warning" type="submit" value="login" />
                             </div>
-                            <p><Link className="text-orange-950 font-bold" to="/registration">Please Registration</Link></p>
                         </form>
+                        <p><Link className="text-orange-950 font-bold" to="/registration">Please Registration</Link></p>
                     </div>
                 </div>
             </div>
